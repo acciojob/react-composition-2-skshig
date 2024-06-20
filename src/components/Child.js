8e5e34d67b9491895fa1e1ef501ec57c2bb15d2b
skshig <skshig@gmail.com>
2024-06-20 13:10:43 +0530

    4

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 
-const Child = ({showProp,setShowProp }) => {
+const Child = ({setShowProp }) => {
 
     function handleClsoe(){
         setShowProp(false);
     }
   return (
     <div>
-        <p className="model-p"></p>
         <button className='model-close' onClick={handleClsoe}>Close Modal</button>
-        <p>this is content of a modal</p>
+        <p className="model p">this is content of a modal</p>
     </div>
   )
 }
